2dfe2988506e72c9dc1c1c9439f43d159450855c
Ryuji Ganaha <ryujignh@gmail.com>
2020-01-14 13:01:40 +0900

    clean up code [ci skip]

--- longest_common_subsequence.rb
+++ longest_common_subsequence.rb
@@ -21,17 +21,22 @@ class LongestCommonSubsequence < Minitest::Test
     if i1 == s1.size || i2 == s2.size
       return 0
     end
+
+    c1 = 0
+    c2 = 0
     c3 = 0
 
     if s1[i1] == s2[i2]
-      c3 = 1 + findCSLengthAux(s1, s2, i1 + 1, i2 + 1)
+      puts "getting c1"
+      c1 = 1 + findCSLengthAux(s1, s2, i1 + 1, i2 + 1)
+    else
+      # Increase index of 2nd string
+      puts "getting c2"
+      c2 = findCSLengthAux(s1, s2, i1, i2 + 1)
+      # Increase index of 1nd string
+     puts "getting c3"
+      c3 = findCSLengthAux(s1, s2, i1 + 1, i2)
     end
-
-    # Increase index of 2nd string
-    c1 = findCSLengthAux(s1, s2, i1, i2 + 1)
-
-    # Increase index of 1nd string
-    c2 = findCSLengthAux(s1, s2, i1 + 1, i2)
 
     [c1, c2, c3].max
   end
